b98edcd6a4e4a55f7b1158e57e39d8bafe1eec24
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-07-11 13:37:53 +0530

    basic version of img to png created

--- image_convertor.py
+++ image_convertor.py
@@ -1,31 +1,38 @@
 import os
 import sys
 from PIL import Image
-# Note this program will change all of the files in the desired directory
+# Note this program will change all the files in the desired directory
 
 # I will be assuming I am only inputting image file for now
 def main():
+    path = path_input()
+    # print(os.listdir(sys.argv[1]))
+    # convertor(filepath, ext)
+    # sys.argv[0]
+
+def path_input():
+
+    try:
+        if len(sys.argv) == 3 :
+            if os.path.isfile(sys.argv[1]):
+                filepath = sys.argv[1]
+            else:
+                sys.exit("Please provide valid path")
+
+        else:
+            sys.exit("Please use only one argument which should be the file path")
+        return filepath
+    except IndexError:
+        sys.exit("Please provide valid details.")
+
+def convertor(filepath,ext):
     folder = os.getcwd()
     for filepath in os.listdir():
         if filepath.endswith('.jpg'):
             f = Image.open(filepath)
             fname, fext = os.path.splitext(filepath)
-            f.save(f"pngs/{fname}.png" )
+            f.save(f"images/{fname}.{ext}")
 
-# def path():
-#     try:
-#         ofile = sys.argv[1]
-#         nfile = sys.argv[2]
-#     except IndexError:
-#         print("please give both original file name as well as the file to be converted")
-# def convertor(ext):
-#
-#     folder = os.getcwd()
-#     os.mkdir("images")
-#     for file in os.listdir():
-#         if file.endswith(ext):
-#             with open(file) as file:
-#                 fname,fext = os.path.splitext(file)
 
 
 
